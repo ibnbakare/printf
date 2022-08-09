2cd5553b58c06b6f6fb0b1d82c7ea654704f3adb
ridhwan <ridmartrite@gmail.com>
2022-08-09 19:34:12 +0100

    ading

--- get_width.c
+++ get_width.c
@@ -5,13 +5,14 @@
  * @format: Formatted string in which to print the arguments.
  * @i: List of arguments to be printed.
  * @list: list of arguments.
- *
+ *ad
  * Return: width.
  */
 int get_width(const char *format, int *i, va_list list)
 {
 	int curr_i;
 	int width = 0;
+
 
 	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
