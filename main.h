121304c17eea8307af513ddcd4951bb6626fb0ff
ridhwan <ridmartrite@gmail.com>
2022-08-09 11:46:54 +0100

    adding specifier functions

--- main.h
+++ main.h
@@ -3,7 +3,21 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
+#define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
+
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/* SIZES */
+#define S_LONG 2
+#define S_SHORT 1
+
+/*
 
 /**
  * struct fmt - Struct op
@@ -18,15 +32,16 @@ struct fmt
 };
 
 typedef struct fmt fmt_t;
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
+	int flags, int width, int precision, int size)
+
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_string(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_percent(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
-
-
-
-
+int print_int(va_list types, char buffer[],
+	int flags, int width, int precision, int size);
 
 #endif
