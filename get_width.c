fcc30bc3e5a07109151e27e7f0e1582a7ad98157
ridhwan <ridmartrite@gmail.com>
2022-08-09 19:38:22 +0100

    ading

--- get_width.c
+++ get_width.c
@@ -3,7 +3,7 @@
 /**
  * get_width - Calculates the width for printing
  * @format: Formatted string in which to print the arguments.
- * @i: List of arguments to be printed.
+ * @i: List of arguments to be printed ok
  * @list: list of arguments.
  *ad
  * Return: width.
