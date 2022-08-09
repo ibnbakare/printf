2145fece282ca62d7e8a02dc851121046494c7ae
Osiyoku Oluwatimilehin <osiyokuemmanuel@gmail.com>
2022-08-09 09:49:23 +0100

    Print f file changes

--- _printf.c
+++ _printf.c
@@ -1,52 +1,5 @@
 #include "main.h"
 
-<<<<<<< HEAD
-int _printf(const char *format, ...)
-{
-	int i = 0;
-	int printed = 0;
-	va_list args;
-
-	va_start(args, format);
-	while (format[i])
-	{
-		while (format[i] == '%')
-		{
-			switch (format[i + 1])
-			{
-				case 'c':
-					printed += print_char(args);
-					i += 2;
-					break;
-				case '%':
-					_putchar('%');
-					printed++;
-					i += 2;
-					break;
-				case 's':
-					printed += print_string(args);
-					i += 2;
-					break;
-				default:
-					_putchar(format[i]);
-					_putchar(format[i + 1]);
-					i += 2;
-			}
-		}
-
-		if (format[i])
-		{
-			_putchar(format[i]);
-			printed++;
-		}
-
-		i++;
-	}
-	va_end(args);
-
-	return (printed);
-}
-=======
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
@@ -111,4 +64,3 @@ void print_buffer(char buffer[], int *buff_ind)
 
 	*buff_ind = 0;
 }
->>>>>>> 55b6a9564b1bc40bde71ae89ce105b16b62a231f
